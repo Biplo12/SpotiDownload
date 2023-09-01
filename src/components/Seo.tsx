061fa79c20da105c132fda32bee59a3ce3c3866c
Robert Siński <75808735+Biplo12@users.noreply.github.com>
2023-09-02 00:28:58 +0200

    chore: meta site url change

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,20 +1,14 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-// !STARTERCONF Change these default meta
 const defaultMeta = {
   title: 'Spotify Playlist Scrapper',
   siteName: 'Spotify Playlist Scrapper',
   description:
     'Scrap Spotify playlist and get the list of songs in the playlist',
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://spotify-playlist-scrapper.vercel.app/',
   type: 'website',
   robots: 'follow, index',
-  /**
-   * No need to be filled, will be populated with openGraph function
-   * If you wish to use a normal image, just specify the path below
-   */
   image: 'https://tsnext-tw.thcl.dev/images/large-og.png',
 };
 
@@ -32,16 +26,6 @@ export default function Seo(props: SeoProps) {
   meta['title'] = props.templateTitle
     ? `${props.templateTitle} | ${meta.siteName}`
     : meta.title;
-
-  // Use siteName if there is templateTitle
-  // but show full title if there is none
-  // !STARTERCONF Follow config for opengraph, by deploying one on https://github.com/theodorusclarence/og
-  // ? Uncomment code below if you want to use default open graph
-  // meta['image'] = openGraph({
-  //   description: meta.description,
-  //   siteName: props.templateTitle ? meta.siteName : meta.title,
-  //   templateTitle: props.templateTitle,
-  // });
 
   return (
     <Head>
